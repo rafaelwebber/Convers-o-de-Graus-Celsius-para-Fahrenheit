e032bed78c71404f3f64a0cadac57814fc31069c
Rafael <163940609+rafaelwebber@users.noreply.github.com>
2024-03-20 13:03:19 -0300

    Add files via upload

---
+++ exercicio4.py
@@ -0,0 +1,15 @@
+#Você está no Brasil, e para temperatura usamos o grau Celsius. Porém, quando você
+#for contrato para trabalhar como programador Python no exterior, deverá usar graus
+#Fahrenheit
+#Ou seja, você fornece a temperatura em graus Celsius, e seu script faz a conversão para
+#graus Fahrenheit
+import math
+
+C = float(input("Digite a temperatura atual em graus Celsius:"))
+
+M = C * 33.8
+
+print(f"\n A temperatura em graus Fahrenheit é: {M:.2f}\n \nE a temperatura em Celsius é:{C:.2f}\n")
+
+
+
